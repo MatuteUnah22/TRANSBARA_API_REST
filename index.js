3c33160e2f857343be4eef50f2e14e2c671a034b
MatuteUnah22 <matute.david@unah.hn>
2022-07-16 22:35:22 -0600

    segundo commit, agregando carpeta routers

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+const express = require('express');
+const app = express();
+const conexion = require("mysql");
+require('dotenv').config();
+const routes = require('./routers/empleados')
+
+
+// constante para el paquete bodyparser
+const bp = require('body-parser');
+
+//Conexion a la base de datos
+const conn = conexion.createConnection(
+    {
+        host: '142.44.161.115',
+        user: 'TRANSBARA',
+        password: 'Trans$$532',
+        database: 'TRANSBARA',
+        port: '3306',
+        multipleStatements: true
+    }
+);
+
+// test de conexion a bd
+conn.connect((err)=>{
+    if(!err) {
+       console.log('CONEXIÓN EXITOSA TRANS. BARAHONA MORAZÁN');
+    } else {
+       console.log('ERROR AL CONECTAR A LA BD');
+    }
+});
+
+// MIDDLEWARES
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
+
+// ROUTES
+app.get('/', (req, res) => {
+   res.send('API Rest del Módulo de Persona - Empleados');
+});
+
+app.use('/empleados', routes);
+
+
+
+//EJECUTAMOS EL SERVER EN UN PUERTO ESPECFICO; PUERTO 3000 (ELSERVICIO NODEJS)
+app.listen(3000,()=> console.log('server running puerto: 3000'));
+
+module.exports = app;
+/* -----------------------------------------------------------------------------
 // conexion bd
 
 // constante para el paquete de mysql
@@ -43,4 +92,4 @@ app.use(bp.json());
 });
 
 //EJECUTAMOS EL SERVER EN UN PUERTO ESPECFICO; PUERTO 3000 (ELSERVICIO NODEJS)
-app.listen(3000,()=> console.log('server running puerto: 3000'));
+app.listen(3000,()=> console.log('server running puerto: 3000'));*/
--- users.js
+++ users.js
@@ -3,8 +3,11 @@ const app = express();
 const conexion = require("mysql");
 require('dotenv').config();
 
+// constante para el paquete bodyparser
+const bp = require('body-parser');
+
 //Conexion a la base de datos
-var conn = conexion.createConnection(
+const conn = conexion.createConnection(
     {
         host: '142.44.161.115',
         user: 'TRANSBARA',
@@ -18,27 +21,43 @@ var conn = conexion.createConnection(
 // test de conexion a bd
 conn.connect((err)=>{
     if(!err) {
-       console.log('CONEXION EXITOSA TRANS. BARAHONA MORAZAN');
+       console.log('CONEXIÓN EXITOSA TRANS. BARAHONA MORAZÁN');
     } else {
        console.log('ERROR AL CONECTAR A LA BD');
-
     }
-
 });
 
 app.get('/', (req, res) => {
-   res.send('Módulo de Servicios');
+   res.send('API Rest del Módulo de  Persona - Usuarios');
 });
 
 //EJECUTAMOS EL SERVER EN UN PUERTO ESPECFICO; PUERTO 3000 (ELSERVICIO NODEJS)
 app.listen(3000,()=> console.log('server running puerto: 3000'));
 
+// enviando los datos JSON  a nodejs api
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
+
+// Obtener todos los usuarios
 app.get("/usuarios", (req, res) => {
-   const consulta = 'call TRANSBARA.SELECT_USUARIO' || {};
-  // const { usr_cod_usuario, usr_password, usr_nom_usuario, usr_cod_empleado, usr_cod_estatus, usr_loguead, usr_cod_tipo_usuario, usr_fec_adicion, usr_usr_adicion, usr_fec_modificacion, usr_usr_modificacion, usr_fec_ult_conex } = req.body;
-   // const consulta = `call SELECT_USUARIO('${usr_cod_usuario}',${usr_password},${usr_nom_usuario},${usr_cod_empleado},${usr_cod_estatus},${usr_loguead},${usr_cod_tipo_usuario},${usr_fec_adicion},${usr_usr_adicion},${usr_fec_modificacion},${usr_usr_modificacion},${usr_fec_ult_conex})`;
-      
-   conn.query(consulta, (error, results) => {
+    const consulta = 'call TRANSBARA.SELECT_TODOS_USUARIO';
+    conn.query(consulta, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send("No pudo traer ningún dato de la BD");
+        }    
+        
+    });
+
+});
+
+// Obtener un usuario con un parametro en específico
+app.get("/usuario/:PI_usr_cod_usuario", (req, res) => {
+    const {PI_usr_cod_usuario} = req.params;
+    const consulta = 'call TRANSBARA.SELECT_USUARIO(?)';
+    conn.query(consulta, [PI_usr_cod_usuario], (error, results) => {
         if (error) throw error;
         if (results.length > 0) {
             res.json(results);
@@ -46,24 +65,29 @@ app.get("/usuarios", (req, res) => {
             res.send("No pudo traer ningún dato de la BD");
         }
     });
+ 
+});
+
+// INSERTAR UN USUARIO CON EL MÉTODO POST
+app.post("/post_usuario", (req, res) => {
+    //const consulta = 'call TRANSBARA.SELECT_USUARIO';
+    //const consulta = 'select * from pe_usuarios';
+    const { usr_cod_usuario, usr_password, usr_nom_usuario, usr_cod_empleado, usr_cod_estatus, usr_loguead, usr_cod_tipo_usuario, usr_fec_adicion, usr_usr_adicion, usr_fec_modificacion, usr_usr_modificacion, usr_fec_ult_conex } = req.body;
+    const consulta = `call TRANSBARA.INSERT_USUARIO( ${PI_usr_cod_usuario},${PV_usr_password},${PV_usr_nom_usuario},${PI_usr_cod_empleado},${PI_usr_cod_estatus},${PV_usr_loguead},${PI_usr_cod_tipo_usuario},${now()},${PV_usr_usr_adicion},${now()},${PV_usr_usr_modificacion},${now()});`;
+    conn.query(consulta, (error, results) => {        
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send("No pudo traer ningún dato de la BD");
+        }    
+        
+    });
 
 });
 
-app.get("/usuarios/:PI_usr_cod_usuario", (req, res) => {
-    const {PI_usr_cod_usuario} = req.params;
-    const consulta = 'call TRANSBARA.SELECT_USUARIO(?)';
-    conn.query(consulta, [PI_usr_cod_usuario], (error, results) => {
-         if (error) throw error;
-         if (results.length > 0) {
-             res.json(results);
-         } else {
-             res.send("No pudo traer ningún dato de la BD");
-         }
-     });
- 
- });
-
- app.get("/eliminarunusuario/:PV_usr_cod_usuario", (req, res) => {
+// ELIMINAR USUARIO CON EL MÉTODO DELETE
+app.get("/eliminarunusuario/:PV_usr_cod_usuario", (req, res) => {
     const {PV_usr_cod_usuario} = req.params;
     const consulta = 'call TRANSBARA.DELETE_USUARIO(?)';
     conn.query(consulta, [PV_usr_cod_usuario], (error, results) => {
@@ -74,10 +98,25 @@ app.get("/usuarios/:PI_usr_cod_usuario", (req, res) => {
             //console.log(results[0][0].pe_usuarios);
             console.log("Usuario eliminado...");
         } else {
-            res.send("No pudo traer ningún dato de la BD");
+            res.send("No pudo eliminar ningún dato de la BD");
         }*/
     });
  
- });
+});
+
+// Método para actualizar un usuario
+app.get("/usuarioUpdate/:PI_usr_cod_usuario", (req, res) => {
+    const {PI_usr_cod_usuario} = req.params;
+    const consulta = 'call TRANSBARA.UPDATE_USUARIO(?);';
+    conn.query(consulta, [PI_usr_cod_usuario], (error, results) => {
+        if (error) {//throw error;
+            // if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send("No pudo traer ningún dato de la BD");
+        }
+    });
+ 
+});
 
 module.exports = app;
